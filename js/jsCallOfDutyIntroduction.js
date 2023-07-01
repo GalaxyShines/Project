66f1353e590e4eb0918052b8ab93cc02557063ec
GalaxyShines <865710022@qq.com>
2023-07-01 08:49:05 +0800

    change

--- js/jsCallOfDutyIntroduction.js
+++ js/jsCallOfDutyIntroduction.js
@@ -1,11 +1,4 @@
-// var ulFirst1=document.getElementsByClassName("ulFirst")[0];
-// ulFirst1.addEventListener("mouseover",function(){
-//     var action=ulFirst1.style.transform;
-    
-// });
-var ulFirst1 = document.getElementsByClassName("ukFirst")[0];
-ulFirst1.addEventListener("mouseover", function() {
-    var currentPosition = parseInt(getComputedStyle(ulFirst1).getPropertyValue('transform').split(',')[4]);
-    var newPosition = currentPosition + 5;
-    ulFirst1.style.transform = "translateX(" + newPosition + "px)";
+var ulFirst1=document.getElementsByClassName("ulFirst")[0];
+ulFirst1.addEventListener("mouseover",function () {
+    ulFirst1.style.transform="translate(+10px)";
 });
