28bf8d4fe53d35b00523c5b7bbbdbb0451c2fe83
GalaxyShines <865710022@qq.com>
2023-07-01 08:55:40 +0800

    11

--- js/jsCallOfDutyIntroduction.js
+++ js/jsCallOfDutyIntroduction.js
@@ -3,3 +3,6 @@ var liAll=ulFirst1.querySelectorAll("li");
 liAll.addEventListener("mouseover",function () {
     liAll.style.transform="translate(+10px)";
 });
+liAll.addEventListener("mouseout",function () {
+    liAll.style.transform="translate(-10px)";
+});
