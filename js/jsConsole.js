f4c4d177a72884a6aefd7544b5e872ef62b8e73a
GalaxyShines <865710022@qq.com>
2023-08-09 16:10:05 +0800

    changes

--- js/jsConsole.js
+++ js/jsConsole.js
@@ -1,1 +1,14 @@
-var labelSet=document.getElementById('set-index-label');
+let labelSet=document.getElementById('set-index-label');
+let cssSet=document.getElementById('set-index-css');
+function getId(id){
+    let getElement=document.getElementById(id);
+    return getElement;
+}
+labelSet.addEventListener('click',function () {
+    getId('show-2').style.display="none";
+    getId('show-1').style.display="block";
+});
+cssSet.addEventListener('click',function () {
+    getId('show-1').style.display="none";
+    getId('show-2').style.display="block";
+});
